1ee20a3aad3147f0d8e290ef872f4594718da7ad
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2014-06-21 21:54:02 +0900

    use StringIO

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,9 +1,10 @@
 class AdminController < ApplicationController
   def dump
-    f = open(File::NULL, 'w')
-    sql = Page.all.to_sql_insert({out: f})
-    send_data(sql.join("\n"), filename: 'wikin_insert.sql')
+    sio = StringIO.new('', 'r+')
+    Page.all.to_sql_insert({out: sio})
+    sio.rewind
+    send_data(sio.read, filename: 'wikin_insert.sql')
   ensure
-    f.close
+    sio.close
   end
 end
