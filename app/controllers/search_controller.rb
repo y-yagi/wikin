de28a98ac1445b242ac544308cf09e907e66e182
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2014-07-31 11:01:11 +0900

    search parameter name change  to "query" by "search"

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,14 +1,14 @@
 class SearchController < ApplicationController
   def index
     @pages = []
-    return if params[:search].blank?
+    return if params[:query].blank?
 
     if request.format.json?
       renderer = Redcarpet::Render::HTML.new(hard_wrap: true)
       @markdown = Redcarpet::Markdown.new(renderer, autolink: true, tables: true)
     end
 
-    @query = params[:search]
+    @query = params[:query]
     @pages = Page.where("title like ? OR body like ?", "%#{@query}%", "%#{@query}%")
   end
 end
