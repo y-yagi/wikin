ba5e34ecfb22e0125687f4abeff8f8160a4ae64b
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2014-07-31 14:09:52 +0900

    change search URL to the appropriate URL

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   root 'index#index'
 
   get 'admin/dump'
-  get 'search/index'
   resources :pages do
     get :titles, on: :collection
+    get :search, on: :collection
   end
+
   get '*path', to: 'pages#show', constraints: Constraints::PathConstraint.new
 end
