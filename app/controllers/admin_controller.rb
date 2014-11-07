d0509e01100cb1582d3e5ecc7f88a73a01e63fd9
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2014-11-07 12:43:39 +0900

    add sequence id change query to dump data

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,6 +2,7 @@ class AdminController < ApplicationController
   def dump
     sio = StringIO.new('', 'r+')
     Page.all.to_sql_insert({out: sio})
+    sio.write("SELECT SETVAL('pages_id_seq', #{Page.last.id});")
     sio.rewind
     send_data(sio.read, filename: 'wikin_insert.sql')
   ensure
