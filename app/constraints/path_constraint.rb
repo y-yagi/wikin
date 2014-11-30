218e2fbc139f1ff86fd866879f122317e511b496
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2014-11-30 11:30:45 +0900

    use `Array#exclude?` instead of `Array#include?`

--- app/constraints/path_constraint.rb
+++ app/constraints/path_constraint.rb
@@ -1,6 +1,6 @@
 class PathConstraint
   def matches?(request)
     path = request.path.split('/').reject(&:empty?).first
-    !Page::INVALID_TITLE_PATTERN.include?(path)
+    Page::INVALID_TITLE_PATTERN.exclude?(path)
   end
 end
