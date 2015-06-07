59bf6b70e2635b36887c972d77a17425f2b7c5af
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2015-06-07 11:55:36 +0900

    modify to output the information of `Notification` to a separate log file

--- config/initializers/nofitication.rb
+++ config/initializers/nofitication.rb
@@ -1,9 +1,12 @@
 if Rails.env.development?
-  ActiveSupport::Notifications.subscribe "instantiation.active_record" do |name, started, finished, unique_id, data|
-    Rails.logger.info("[Notifications #{name}] time: #{(finished - started).to_f}  data: #{data.inspect}")
+  logger = ActiveSupport::Logger.new(File.join(Rails.root, "log", "notifications.log"))
+  ActiveSupport::Notifications.subscribe "instantiation.active_record" do |*args|
+    event = ActiveSupport::Notifications::Event.new(*args)
+    logger.info("[#{event.name}] time: #{event.duration.to_f}  data: #{event.payload.inspect}")
   end
 
-  ActiveSupport::Notifications.subscribe "sql.active_record" do |name, started, finished, unique_id, data|
-    Rails.logger.info("[Notifications #{name}] time: #{(finished - started).to_f}  data: #{data.inspect}")
+  ActiveSupport::Notifications.subscribe('sql.active_record') do |*args|
+    event = ActiveSupport::Notifications::Event.new(*args)
+    logger.info("[#{event.name}] time: #{event.duration.to_f}  data: #{event.payload.inspect}")
   end
 end
