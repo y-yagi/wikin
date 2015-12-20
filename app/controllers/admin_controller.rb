e9f1c31476bb7d9973cfc8a2f87859497ac90ca9
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2015-12-20 13:41:00 +0900

    convert to array

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -3,7 +3,7 @@ class AdminController < ApplicationController
     sio = StringIO.new('', 'r+')
     return redirect_to root_path unless Page.exists?
 
-    Page.all.to_sql_insert({out: sio})
+    Page.all.to_a.to_sql_insert({out: sio})
     sio.write("SELECT SETVAL('pages_id_seq', #{Page.last.id});")
     sio.rewind
     send_data(sio.read.remove("\r"), filename: 'wikin_insert.sql')
