2413463d22948b3d539608848783d29c203e83c0
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2016-04-28 16:24:41 +0900

    do nothing when id is empty

--- app/channels/page_channel.rb
+++ app/channels/page_channel.rb
@@ -1,6 +1,8 @@
 # Be sure to restart your server when you modify this file. Action Cable runs in a loop that does not support auto reloading.
 class PageChannel < ApplicationCable::Channel
   def subscribed
+    return if params[:id].blank?
+
     @page = Page.find(params[:id])
     stream_for(@page)
     PageChannel.broadcast_to @page, { connection_count: ActionCable.server.connections.count }
