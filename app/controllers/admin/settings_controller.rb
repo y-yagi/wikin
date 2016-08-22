7c9ec3c3e5068e8aa3c46254465e5271776cbd7c
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2016-08-22 09:37:33 +0900

    implement settings

--- app/controllers/admin/settings_controller.rb
+++ app/controllers/admin/settings_controller.rb
@@ -1,7 +1,20 @@
 class Admin::SettingsController < ApplicationController
+  before_action :set_setting , only: [:edit, :update]
   def edit
+    @setting = Setting.first_or_create
   end
 
   def update
+    @setting.update(setting_params)
+    render :edit
   end
+
+  private
+    def set_setting
+      @setting = Setting.first_or_create
+    end
+
+    def setting_params
+      params.require(:setting).permit(:enable_one_time_password)
+    end
 end
