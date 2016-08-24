7b2b827be569048ec51152cb363f2a5b1d41fd49
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2016-08-25 07:52:09 +0900

    set password if enable one time password

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -1,2 +1,13 @@
 class Setting < ApplicationRecord
+  before_save :set_one_time_password_secret
+
+  def set_one_time_password_secret
+    if enable_one_time_password && one_time_password_secret.blank?
+      one_time_password_secret = Rails.application.message_verifier(:one_time_password_secret).generate(ROTP::Base32.random_base32)
+    end
+  end
+
+  def original_one_time_password_secret
+    Rails.application.message_verifier(:one_time_password_secret).verify(one_time_password_secret)
+  end
 end
