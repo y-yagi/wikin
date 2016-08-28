8a4d0a574da51321174e835688cfa3b49410fcc7
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2016-08-29 08:45:10 +0900

    add test for setting

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -2,8 +2,8 @@ class Setting < ApplicationRecord
   before_save :set_one_time_password_secret
 
   def set_one_time_password_secret
-    if enable_one_time_password && one_time_password_secret.blank?
-      one_time_password_secret = Rails.application.message_verifier(:one_time_password_secret).generate(ROTP::Base32.random_base32)
+    if self.enable_one_time_password && self.one_time_password_secret.blank?
+      self.one_time_password_secret = Rails.application.message_verifier(:one_time_password_secret).generate(ROTP::Base32.random_base32)
     end
   end
 
--- test/models/setting_test.rb
+++ test/models/setting_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class SettingTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "set one time password secret" do
+    setting = Setting.new
+    setting.enable_one_time_password = true
+    setting.save!
+
+    assert setting.reload.one_time_password_secret
+  end
+
+  test "restore secret" do
+    setting = Setting.new
+    setting.enable_one_time_password = true
+    setting.save!
+
+    assert_nothing_raised do
+      setting.original_one_time_password_secret
+    end
+  end
 end
