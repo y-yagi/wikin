4304c9ef791554b32156f3da8febf39020eb8ce7
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2017-03-27 07:46:13 +0900

    move assets path setting to assets initializer

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,3 +4,5 @@ Rails.application.config.assets.precompile += %w(
   bootstrap-theme-white-plum/dist/fonts/glyphicons-halflings-regular.ttf
 )
 Rails.application.config.assets.version = '1.0'
+
+Rails.application.config.assets.paths << Rails.root.join('node_modules')
