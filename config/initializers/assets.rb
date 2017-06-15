dbb76b4e6f8416c3506249e7ee509319ea554b77
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2017-06-16 07:22:34 +0900

    only add graphiql in develop env

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,6 +3,14 @@ Rails.application.config.assets.precompile += %w(
   bootstrap-theme-white-plum/dist/fonts/glyphicons-halflings-regular.eot
   bootstrap-theme-white-plum/dist/fonts/glyphicons-halflings-regular.ttf
 )
+
+if Rails.env.development?
+Rails.application.config.assets.precompile += %w(
+  graphiql/rails/application.css
+  graphiql/rails/application.js
+)
+end
+
 Rails.application.config.assets.version = '1.0'
 
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
