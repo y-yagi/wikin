dcd800023af7f4e82fbb2b615a18df6984a344b8
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2018-04-25 16:18:47 +0900

    run `db:migrate`

--- db/schema.rb
+++ db/schema.rb
@@ -10,29 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160620000744) do
+ActiveRecord::Schema.define(version: 2016_06_20_000744) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "old_pages", force: :cascade do |t|
-    t.text     "body"
-    t.integer  "page_id"
+  create_table "old_pages", id: :serial, force: :cascade do |t|
+    t.text "body"
+    t.integer "page_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer  "parent_id"
-    t.string   "title"
-    t.string   "tags",                    array: true
-    t.index ["page_id"], name: "index_old_pages_on_page_id", using: :btree
+    t.integer "parent_id"
+    t.string "title"
+    t.string "tags", array: true
+    t.index ["page_id"], name: "index_old_pages_on_page_id"
   end
 
-  create_table "pages", force: :cascade do |t|
-    t.string   "title"
-    t.text     "body"
-    t.integer  "parent_id"
+  create_table "pages", id: :serial, force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.integer "parent_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "tags",       array: true
+    t.string "tags", array: true
   end
 
 end
