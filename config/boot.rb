e21754d22e6ffa2792701caa924cf1d74619eb88
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2018-05-21 08:07:37 +0900

    disable compile cache iseq

--- config/boot.rb
+++ config/boot.rb
@@ -2,4 +2,16 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' if File.exist?(ENV['BUNDLE_GEMFILE'])
-require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
+
+require 'bootsnap'
+env = ENV['RAILS_ENV'] || 'development'
+
+Bootsnap.setup(
+  cache_dir: 'tmp/cache',
+  development_mode: env == 'development',
+  load_path_cache: true,
+  autoload_paths_cache: true,
+  disable_trace: true,
+  compile_cache_iseq: false,
+  compile_cache_yaml: true
+)
