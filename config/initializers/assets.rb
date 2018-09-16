6a9377a02108cba85884ec8935d1b3dcaf298b5b
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2018-09-17 08:15:39 +0900

    use serviceworker via `serviceworker-rails`

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,6 +2,8 @@ if Rails.env.development?
 Rails.application.config.assets.precompile += %w(
   graphiql/rails/application.css
   graphiql/rails/application.js
+  serviceworker.js
+  manifest.json
 )
 end
 
