4f407ba54cff14fbcc348d0a00ada51bf6938183
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2018-09-17 11:07:17 +0900

    serviceworker.js and manifest.json are need to compile all env

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,12 +1,11 @@
 if Rails.env.development?
-Rails.application.config.assets.precompile += %w(
-  graphiql/rails/application.css
-  graphiql/rails/application.js
-  serviceworker.js
-  manifest.json
-)
+  Rails.application.config.assets.precompile += %w(
+    graphiql/rails/application.css
+    graphiql/rails/application.js
+  )
 end
 
+Rails.application.config.assets.precompile += %w(serviceworker.js manifest.json)
 Rails.application.config.assets.version = '1.0'
 
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
