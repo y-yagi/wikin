6980af14c21b1bad3ffd0495aad9431eb20691da
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2019-04-03 12:58:57 +0900

    use `devbridge-autocomplete`

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,9 +3,9 @@ require("turbolinks").start()
 
 var $ = require("jquery")
 window.$ = $;
-window.$.autocomplete = require("jquery-ui/ui/widgets/autocomplete")
 
 require("bootstrap")
+require("devbridge-autocomplete")
 
 require("./pages")
 require("./serviceworker-companion")
