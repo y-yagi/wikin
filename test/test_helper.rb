147f726d7b943a6e2ef8de1992805513fea0f984
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2019-07-04 08:49:18 +0900

    Allow to disable minitest-sound

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -39,6 +39,8 @@ class ActionDispatch::IntegrationTest
   Capybara.javascript_driver = :selenium_chrome_headless
 end
 
-Minitest::Sound.success = '/home/yaginuma/Dropbox/tmp/music/other/sey.mp3'
-Minitest::Sound.failure = '/home/yaginuma/Dropbox/tmp/music/other/mdai.mp3'
-Minitest::Sound.during_test = '/home/yaginuma/Dropbox/tmp/music/other/rs1_25_beatthemup.mp3'
+if !ENV["DISABLE_MINITEST_SOUND"]
+  Minitest::Sound.success = '/home/yaginuma/Dropbox/tmp/music/other/sey.mp3'
+  Minitest::Sound.failure = '/home/yaginuma/Dropbox/tmp/music/other/mdai.mp3'
+  Minitest::Sound.during_test = '/home/yaginuma/Dropbox/tmp/music/other/rs1_25_beatthemup.mp3'
+end
