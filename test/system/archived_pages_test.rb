e36ffa8711162492f2471cd00d133ec5244eacb1
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2019-08-07 14:51:09 +0900

    Make work with `apparition`

--- test/system/archived_pages_test.rb
+++ test/system/archived_pages_test.rb
@@ -1,50 +1,47 @@
 require "application_system_test_case"
 
 class ArchivedPagesTest < ApplicationSystemTestCase
-  def host!(_host)
-    super
-    auth = "#{ENV["BASIC_AUTH_USER"]}:#{ENV["BASIC_AUTH_PASSWORD"]}"
-    Capybara.app_host = "http://#{auth}@127.0.0.1"
-  end
-
   setup do
+    auth = ActionController::HttpAuthentication::Basic.encode_credentials(ENV["BASIC_AUTH_USER"], ENV["BASIC_AUTH_PASSWORD"])
+    page.driver.add_headers("Authorization" => auth)
+
     visit root_path
   end
 
   test 'archived pages' do
     click_link 'アーカイブページ一覧'
 
-    assert_match 'archive_paged_include_tags', page.text
+    assert_text 'archive_paged_include_tags'
   end
 
   test 'show archived page' do
     archived_page = archived_pages(:include_tags)
     visit archived_page_path(archived_page)
 
-    assert_match 'archive_paged_include_tags', page.text
+    assert_text 'archive_paged_include_tags'
   end
 
   test 'destroy archived page' do
     archived_page = archived_pages(:include_tags)
     visit archived_page_path(archived_page)
 
-    accept_alert { first("a[title='削除']").click }
+    accept_confirm { first("a[title='削除']").click }
 
     visit archived_page_path(archived_page)
-    assert_match 'not found', page.text
+    assert_text 'not found'
   end
 
   test 'restore archived page' do
     archived_page = archived_pages(:include_tags)
     visit archived_page_path(archived_page)
 
-    accept_alert { first("a[title='リストア']").click }
+    accept_confirm { first("a[title='リストア']").click }
 
     visit pages_path
-    assert_match 'archive_paged_include_tags', page.text
+    assert_text 'archive_paged_include_tags'
 
     click_link 'アーカイブページ一覧'
-    assert_no_match 'archive_paged_include_tags', page.text
+    assert_no_text 'archive_paged_include_tags'
   end
 end
 
